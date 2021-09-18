53ab04dd9ec810ba0e9fe5f4fc8ec48d3072f52d
Wilhams Junior <wilhamsmeira@gmail.com>
2021-09-18 17:39:41 -0300

    adding events to the canvas

--- Desenhando com canvas/d7js_drawing_base/script.js
+++ Desenhando com canvas/d7js_drawing_base/script.js
@@ -1,13 +1,30 @@
 let currentColor = 'black';
+let canDraw = false; // canDraw -> tradução seria pode desenhar
+
 // Para trabalhar com canvas primeiro temos que selecionar ele pela id e depois temos que pegar o contexto
 // esse contexto é pego em 2d para aplicar os desenhos, podemos trabalghar tbm com 3d mas aí já é um assunto
 // mais particular sobre o canvas e o foco é JS
 let screen = document.querySelector('#tela');
-let contexto = screen.get.Context('2d')
+let contexto = screen.getContext('2d')
 
 document.querySelectorAll('.colorArea .color').forEach(item => {
     item.addEventListener('click', colorClickEvent)
 });
+
+/**
+ * Passo a passo para desenhar no canvas:
+ * - Qunado o click do mouse ABAIXAR, ative o modo desenho
+ * - Quando o mouse se MOVER, se o modo desenho estiver ativo, desenhe.
+ * - Quando o click do mouse LEVANTAR(ou o user para de clikar no mouse), desative o modo desenho.
+ */
+
+//  Eventos que detectam variancias no mouse
+// mousedown -> Quando clikamos no mouise
+// mousemove -> quando movemos o mouse
+// mouseup -> quando soltamos o botão cliakdo
+ screen.addEventListener('mousedown', mouseDownEvent);
+ screen.addEventListener('mousemove', mouseMoveEvent);
+ screen.addEventListener('mouseup', mouseUpEvent);
 
 // esta função serve para que quando o usuário escolhe um cor esta cor precisa ter um destaque
 // então aqui estamos setando para que cada cor escolhida tenha esse destaque
@@ -23,4 +40,24 @@ function colorClickEvent(e) {
     document.querySelector('.color.active').classList.remove('active');
     // Adicionando destaque para a cor selecionada
     e.target.classList.add('active');
+}
+
+// Função que escuta quando o user  clika no mouse e ativa o modo desenho canvas
+function mouseDownEvent() {
+    // Setando var canDraw ou 'podeDesenhar' para true
+    canDraw = true;
+}
+// Função que escuta quando o mouse se move, porém ela só vai escutar com uma condição
+// que é se canDraw tiver true
+function mouseMoveEvent() {
+    // Validação para impedir que o usuário desenhe se o mouse tiver apenas se movendo
+    // para desenhar o mouse tem q estar clikado
+    if (canDraw) {
+        
+    }
+}
+// Funcção que escuta quando o user parou de clikar o botão do mouse
+function mouseUpEvent() {
+    // Setando var canDraw ou 'podeDesenhar' para false
+    canDraw = false;
 }
